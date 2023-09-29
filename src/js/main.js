bddfa3f400a6e56b070dd47a5a749523a6d22753
mteresacastro <m.teresacastrogonzalez@gmail.com>
2023-09-29 23:10:42 +0200

    image-not-found-post

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,7 @@ const inputText = document.querySelector('.js-searchText');
 const btnSearch = document.querySelector('.js-searchBtn');
 
 const principalList = document.querySelector('.js-principalList');
-const divBox = document.querySelector('.js-serieBox');
+let divBox = [];
 
 let seriesList = []; 
 let seriesFavourites= [];
@@ -24,27 +24,25 @@ function handleClickBtnSearch(event) {
     return; //sirve para parar y no continuar la funcion.
   }
   const urlAPI = `//api.tvmaze.com/search/shows?q=:${valueInput}`;
-  
+
   fetch(urlAPI)
       .then(response => response.json())
       .then(function (dataAPI){
           seriesList = dataAPI.filter((serie)=>serie.show.name.toLowerCase().includes(valueInput.toLowerCase())); //mapear esto bien
           renderSerieList(seriesList);
-        });   
+        });
   
 }
 
 function handleClickFavourites(event) {
   event.preventDefault();
-  const clickedElement = event.currentTarget.dataset.id;
-  event.target.dataset.id.classList.add('selected');
-
-
-  console.log(event.target);
-
-
+  const clickedElement = event.currentTarget.dataset.idElement;
+  const findSerie = seriesList.find((serie)=>serie.show.id === clickedElement);
+  seriesFavourites.push(findSerie);
+  //clickedElement.classList.add('selected');
+  console.log(clickedElement);
 }
- 
+
 //funciones render
 
 function renderSerie(oneSerie) {
@@ -52,9 +50,13 @@ function renderSerie(oneSerie) {
   const divElement = document.createElement('div');
   divElement.setAttribute('class', 'serieBox js-serieBox');
   divElement.dataset.idElement = oneSerie.show.id; //para saber cual es la id de cada serie
+  divBox = document.querySelector('.js-serieBox');
+  divElement.addEventListener('click', handleClickFavourites);
+  
 
   const imgElement = document.createElement('img');
-  imgElement.setAttribute('class','serieBox__img js-serieBox__img'); 
+  imgElement.setAttribute('class','serieBox__img js-serieBox__img');
+  imgElement.setAttribute('alt', oneSerie.show.name);
   if (oneSerie.show.image === null) {
     imgElement.setAttribute('src', 'https://via.placeholder.com/210x295/ffffff/666666/?text=TV');
   }
@@ -90,6 +92,6 @@ function renderFavourite(){
 //eventos
 
 btnSearch.addEventListener('click', handleClickBtnSearch);
-principalList.addEventListener('click', handleClickFavourites);
+
 
 
